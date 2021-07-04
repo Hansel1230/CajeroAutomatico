8f7b54a4d5658b2ff54b8a0915b7b037aa2518d4
Hansel De los santos <hanseldelossantos@Hansels-MacBook-Pro.local>
2021-07-04 14:37:10 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CajeroAutomatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
