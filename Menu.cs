e4c2fb3e5d53378fa6d07c5e0b44de54cc671b0b
Hansel De los santos <hanseldelossantos@Hansels-MacBook-Pro.local>
2021-07-05 17:48:50 -0400

    Ultimo Commit

--- Menu.cs
+++ Menu.cs
@@ -3,77 +3,108 @@ namespace CajeroAutomatico
 {
     public class Menu
     {
-        static ModoDeDispensacion modo = new ModoDeDispensacion(9);
+        public static int Opcion { get; set; }
+
+        static ModoDeDispensacion modo = new ModoDeDispensacion();
         //int opcion = 0;
+        public Menu()
+        {
+
+        }
         public static void menu()
         {
             Console.WriteLine("Bienvenido a su cajero preferido!!\n\n");
             Console.WriteLine("1-Modo de dispensacion\n2-Retiro de dinero\n\nSeleccione la opcion deseada:");
-            int opcion = Convert.ToInt16(Console.ReadLine());
-            switch (opcion)
-            {
-                case 1:
-                    Console.WriteLine("\nModo de dispensacion\n\n");
-                    menudispensacion();
-                    Console.WriteLine("\n\nPulse enter.");
-                    Console.Clear();
-                    break;
 
-                case 2:
-                    Console.WriteLine("\nRetiro de dinero\n\n");
-                    menuRetiro();
-                    modo.Modo3();
-                    Console.WriteLine("\n\nPara Continuar (1)\nPara salir (2)\n\nSeleccione la opcion deseada:");
-                    int  salir = Convert.ToInt16(Console.ReadLine());
-                    salida(salir);
-                    break;
+            try
+            {
+                int opcion = Convert.ToInt16(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("\nModo de dispensacion\n\n");
+
+                        Console.WriteLine("1-De 200 y 1000\n2-De 100 y 500\n3-Eficiente: 1000,500,200 y 100  \n\nSeleccione la opcion deseada:");
+                        opcion = Convert.ToInt16(Console.ReadLine());
+                        if (opcion > 3)
+                        {
+                            Console.WriteLine("\nDebe ingresar una opcion correcta. Pulse enter");
+                            Console.ReadKey();
+                            
+                        }
+                        //asignacion a propiedad
+                        Opcion = opcion;
+                        Console.WriteLine("\n\nModo guardado. Pulse enter.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nRetiro de dinero\n\n");
+                         menuRetiro();
+                        break;
+
+                    default:
+                        Console.WriteLine("Debe ingresar una opcion valida. Pulse Enter");
+                        Console.ReadKey();
+
+                        break;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Debe ingresar una opcion valida. Pulse Enter");
+                Console.ReadKey();
+                Console.Clear();
+                menu();
             }
             menu();
         }
-        public static void menudispensacion()
-        {
-            Console.WriteLine("1-De 200 y 1000\n2-De 100 y 500\n3-Eficiente: 1000,500,200 y 100  \n\nSeleccione la opcion deseada:");
-            int opcion = Convert.ToInt16(Console.ReadLine());
 
-            switch (opcion)
-            {
-                case 1:
-                    menuRetiro();
-                    modo.Modo1();
-                    Console.ReadKey();
-                    break;
-                case 2:
-                    menuRetiro();
-                    modo.Modo2();
-                    Console.ReadKey();
-                    break;
-                case 3:
-                    menuRetiro();
-                    modo.Modo3();
-                    Console.ReadKey();
-                    break;
-            }
-        }
         public static void menuRetiro()
         {
             Console.WriteLine("\nIngrese cifra de dinero a retirar, referente al modo de dispensacion. Ejemplo:1000 :");
-            int cifra = Convert.ToInt32(Console.ReadLine());
 
-            if (cifra % 2 == 0 && cifra > 100)
+            int cifra = Convert.ToInt32(Console.ReadLine());
+            if (Opcion == 1)
             {
-                modo.Cifra = cifra;
+                if (cifra % 200 == 0 && cifra > 199)
+                {
+                    modo.Cifra = cifra;
+                    modo.Modo1();
+                    Console.ReadKey();
+                }
+            }
+            
+            if (Opcion == 2)
+            {
+                if (cifra % 2 == 0 && cifra > 99)
+                {
+                    modo.Cifra = cifra;
+                    modo.Modo2();
+                    Console.ReadKey();
+                }
+            }
+            
+            if (Opcion == 3)
+            {
+                if (cifra % 2 == 0 || cifra % 200 == 0 && cifra >= 100)
+                {
+                    modo.Cifra = cifra;
+                    modo.Modo3();
+                    Console.ReadKey();
+                }
             }
             else
             {
-                Console.WriteLine("\nDebe ingresar una cifra adecuada al monto de dispensacion. Pulse enter");
-                Console.ReadKey();
-                menuRetiro();
+                Error();
             }
         }
-
-
-        public static void salida(int salir)
+        public static void salida()
         {
+            Console.WriteLine("\n\nPara Continuar (1)\nPara salir (2)\n\nSeleccione la opcion deseada:");
+            int salir = Convert.ToInt16(Console.ReadLine());
             switch (salir)
             {
                 case 1:
@@ -85,7 +116,18 @@ namespace CajeroAutomatico
                     Console.ReadKey();
                     Environment.Exit(0);
                     break;
+                default:
+                    Console.WriteLine("Debe ingresar una opcion valida. Pulse Enter");
+                    Console.ReadKey();
+                    salida();
+                    break;
             }
+        }
+        public static  void Error()
+        {
+            Console.WriteLine("\nDebe ingresar una cifra adecuada al monto de dispensacion. Pulse enter");
+            Console.ReadKey();
+            //menuRetiro();
         }
     }
 }
--- ModoDeDispensacion.cs
+++ ModoDeDispensacion.cs
@@ -3,6 +3,7 @@ namespace CajeroAutomatico
 {
     public class ModoDeDispensacion
     {
+        static Menu menu = new Menu();
         public int Valor1 {get;}=1000;
         public int Valor2 {get;}=200;
         public int Valor3 {get;}=500;
@@ -12,39 +13,22 @@ namespace CajeroAutomatico
         int count2 = 0;
         int count3 = 0;
         int count4 = 0;
-
-        public ModoDeDispensacion(int opcion)
+        public ModoDeDispensacion()
         {
              
         }
+
         public void Modo1()
         {
-            while (this.Cifra - this.Valor1 >= 0)
-            {
-                this.Cifra -= this.Valor1;
-                count1++;   
-            }
-            while (this.Cifra - this.Valor2 >= 0)
-            {
-                this.Cifra -= this.Valor2;
-                count2++;
-            }
-            Console.WriteLine("\nSe le dio "+count1+" papeletas de "+Valor1 +", "+count2 +" papeletas de "+Valor2);
+
+            Cuenta(Valor1, Valor2, count1, count2);
         }
+
         public void Modo2()
         {
-            while (this.Cifra - this.Valor3 >= 0)
-            {
-                this.Cifra -= this.Valor3;
-                count3++;
-            }
-            while (this.Cifra - this.Valor4 >= 0)
-            {
-                this.Cifra -= this.Valor4;
-                count4++;
-            }
-            Console.WriteLine("\nSe le dio "+count3+" papeletas de "+Valor3+", "+count4+" papeletas de "+Valor4);
-        }
+            Cuenta(Valor3, Valor4, count3, count4);
+         }
+
         public void Modo3()
         {   //Eficiente
             while (this.Cifra - this.Valor1 >= 0)
@@ -67,15 +51,29 @@ namespace CajeroAutomatico
                 this.Cifra -= this.Valor4;
                 count4++;
             }
-            Console.WriteLine("\nSe le dio "+count1+" papeletas de "+Valor1+", "+count3+" papeletas de "+Valor3+", "+count2+" papeletas de "+Valor2+" y "+count4+" papeletas de "+Valor4);
+            Console.WriteLine("\nSe le dio "+count1+" papeletas de "+Valor1+", "+count3+" papeletas de "+Valor3+", "+count2+" papeletas de "+Valor2+" y "+count4+" papeletas de "+Valor4+ "\n\nPulse Enter.");
+            Console.ReadKey();
+            Console.Clear();
+            Menu.salida();
         }
-        //public void Cuenta(int Valor,int count)
-        //{
-        //    while (this.Cifra -Valor >= 0)
-        //    {
-        //        this.Cifra -=Valor;
-        //        count++;
-        //    }
-        //}
+
+        public void Cuenta(int Valor1,int Valor2,int count1, int count2)
+        {
+            while (this.Cifra - Valor1 >= 0)
+            {
+                this.Cifra -= Valor1;
+                count1++;
+            }
+            //Cuenta(Valor2, count2);
+            while (this.Cifra - Valor2 >= 0)
+            {
+                this.Cifra -= Valor2;
+                count2++;
+            }
+            Console.WriteLine("\nSe le dio " + count1 + " papeletas de " + Valor1 + ", " + count2 + " papeletas de " + Valor2 + "\n\nPulse Enter.");
+            Console.ReadKey();
+            Console.Clear();
+            Menu.salida();
+        }
     }
 }
